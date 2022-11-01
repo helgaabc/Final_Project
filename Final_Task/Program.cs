bd4988e0bbee7fb7eabf0f7570d88e2e4e185a24
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-11-01 11:21:45 +0300

    added solution

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayStrings = new string[size];
+int len = 3;
+int pos = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    if (element.Length <= len)
+    {
+        arrayStrings[pos] = element;
+        pos++;
+    }
+}
+Console.WriteLine();
+PrintArray(arrayStrings);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
